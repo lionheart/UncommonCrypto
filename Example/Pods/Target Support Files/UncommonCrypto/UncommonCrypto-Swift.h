e1fecd61e764c92d2435590b7d316574ba1536f0
Dan Loewenherz <dan@lionheartsw.com>
2016-06-26 14:52:29 -0500

    update pod target support files

---
+++ Example/Pods/Target Support Files/UncommonCrypto/UncommonCrypto-Swift.h
@@ -0,0 +1,13 @@
+//
+//  UncommonCrypto-Swift.h
+//  Pods
+//
+//  Created by Daniel Loewenherz on 6/26/16.
+//
+//
+
+#ifndef UncommonCrypto_Swift_h
+#define UncommonCrypto_Swift_h
+
+
+#endif /* UncommonCrypto_Swift_h */
